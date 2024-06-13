438bbe82b3a40879a49ae065b4ffe7e4cb67e661
eLDoherty <leonardodoherty@gmail.com>
2024-06-13 21:56:48 +0700

    Add testimonials and works

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
     $('.hamburger').on('click', function() {
         $('.menu').toggleClass('active');
     });
+
+    $('.slick').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        utoplaySpeed: 1000,
+      });
 });
